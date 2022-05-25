8ccfa734ba67ff0dc813013fd624a1f1772a4b57
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-05-26 00:53:50 +0600

    MongoDB Setup

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const stripe = require('stripe')(process.env.STRIPE_KEY);
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-var nodemailer = require("nodemailer");
-var sgTransport = require("nodemailer-sendgrid-transport");
 app.use(cors());
 app.use(express.json());
 
