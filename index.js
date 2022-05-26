3621f9e1ae9a5f8801dcd486c47d09b26b2e5c69
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-05-27 05:30:44 +0600

    order payment status

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -35,10 +35,51 @@ function verifyJWT(req, res, next) {
 async function run() {
   try {
     await client.connect();
-    const collection = client.db("test").collection("devices");
     const data = client.db("Marley").collection("products");
     const order = client.db("Marley").collection("orders");
+    const userData = client.db("Marley").collection("user");
+    const paymentData = client.db("Marley").collection("payments");
 
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userData.findOne({ email: requester });
+      if (requesterAccount.role === "admin") {
+        next();
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
+    };
+
+    //payment
+
+    app.post('/create-payment-intent', verifyJWT, async(req, res) =>{
+      const service = req.body;
+      const price = service.price;
+      const amount = price*100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount : amount,
+        currency: 'usd',
+        payment_method_types:['card']
+      });
+      res.send({clientSecret: paymentIntent.client_secret})
+    });
+    //order status
+    app.patch('/order/:id', verifyJWT, async(req, res) =>{
+      const id  = req.params.id;
+      const payment = req.body;
+      const filter = {_id: ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId
+        }
+      }
+      const result = await paymentData.insertOne(payment);
+      const updatedBooking = await appointment.updateOne(filter, updatedDoc);
+      res.send(updatedBooking);
+    })
+
+    //products
     app.get("/resources", async (req, res) => {
       const query = {};
       const cursor = data.find(query);
@@ -66,6 +107,7 @@ async function run() {
       res.send(result);
     });
 
+    //order
     app.post("/order", async (req, res) => {
       const detail = req.body;
       const result = await order.insertOne(detail);
@@ -74,11 +116,9 @@ async function run() {
     });
 
     app.get("/order", async (req, res) => {
-
       const query = {};
       const cursor = order.find(query);
       const result = await cursor.toArray();
-
       res.send(result);
     });
 
@@ -89,6 +129,44 @@ async function run() {
       const booking = await order.findOne(query);
       res.send(booking);
     })
+
+    //user
+    app.get("/user", verifyJWT, async (req, res) => {
+      const users = await userData.find().toArray();
+      res.send(users);
+    });
+
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userData.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
+    app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userData.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userData.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.SECRET, {
+        expiresIn: "12h",
+      });
+      res.send({ result, token });
+    });
 
 
   } finally {
